2594cc4bb18fa48ed73265fb0b71526463aa6ae1
silafarsidia <silafarsidia57@gmail.com>
2022-08-17 18:13:41 +0700

    Creating Capture Activity Screen

---
+++ app/src/main/java/com/example/detectionappliation/CaptureActivity.kt
@@ -0,0 +1,11 @@
+package com.example.detectionappliation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CaptureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capture)
+    }
+}
