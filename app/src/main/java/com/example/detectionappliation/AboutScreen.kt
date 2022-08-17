43210b9b6ea20260171bf03f5840ba560294ef53
silafarsidia <silafarsidia57@gmail.com>
2022-08-17 19:30:37 +0700

    Updating About Activity Screen

--- app/src/main/java/com/example/detectionappliation/AboutScreen.kt
+++ app/src/main/java/com/example/detectionappliation/AboutScreen.kt
@@ -1,11 +1,28 @@
 package com.example.detectionappliation
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.detectionappliation.databinding.ActivityAboutScreenBinding
 
 class AboutScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAboutScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about_screen)
+        binding = ActivityAboutScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.navigationBarColor = resources.getColor(R.color.black)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = this.resources.getColor(R.color.black)
+        }
+
+
     }
 }
