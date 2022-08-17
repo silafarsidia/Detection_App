428f50184b82b11a45ce35420884b664b2b01ebc
silafarsidia <silafarsidia57@gmail.com>
2022-08-17 19:03:15 +0700

    Updating Capture Activity Screen

--- app/src/main/java/com/example/detectionappliation/CaptureActivity.kt
+++ app/src/main/java/com/example/detectionappliation/CaptureActivity.kt
@@ -1,11 +1,21 @@
 package com.example.detectionappliation
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class CaptureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_capture)
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.navigationBarColor = resources.getColor(R.color.black)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = this.resources.getColor(R.color.black)
+        }
     }
 }
