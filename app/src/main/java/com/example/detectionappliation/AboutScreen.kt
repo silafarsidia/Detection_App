40719e53c98ec7e2034d93e4ab210307428e8a98
silafarsidia <silafarsidia57@gmail.com>
2022-08-17 19:21:56 +0700

    Creating About Activity Screen

---
+++ app/src/main/java/com/example/detectionappliation/AboutScreen.kt
@@ -0,0 +1,11 @@
+package com.example.detectionappliation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_screen)
+    }
+}
